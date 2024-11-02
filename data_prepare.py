98f01cd73bf3944f2368de6c5c7cef05fbe5db41
Vivek Rachakonda <viveknani98@gmail.com>
2024-11-02 17:53:42 -0400

    added function to create train/validation dataset

--- data_prepare.py
+++ data_prepare.py
@@ -2,21 +2,24 @@ import torch
 from datasets import load_dataset
 from transformers import AutoTokenizer
 
-BATCH_SIZE = 512
 BLOCK_SIZE = 256
 
-dataset = load_dataset('roneneldan/TinyStories', split='train')
-tokenizer = AutoTokenizer.from_pretrained('gpt2')
-tokenizer.pad_token = tokenizer.eos_token
+def create_dataset(tokenizer_model, block_size, split='train'):
 
-dataset = dataset.map(lambda e: tokenizer(e['text'], 
-                                          truncation=True, 
-                                          padding='max_length', 
-                                          # max_length plus one to get the next tokens
-                                          max_length=BLOCK_SIZE + 1
-                                          ), batched=True)
-dataset.set_format(type='torch', columns=['input_ids', 'attention_mask'])
-dataset.save_to_disk('tokenized_tinystories')
+    dataset = load_dataset('roneneldan/TinyStories', split=split)
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_model)
+    
+    # gpt2 does not have padding token, so using eos as padding token
+    if tokenizer_model == 'gpt2':
+        tokenizer.pad_token = tokenizer.eos_token
 
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE)
-print(next(iter(dataloader)))
+    dataset = dataset.map(lambda e: tokenizer(e['text'], 
+                                            truncation=True, 
+                                            padding='max_length', 
+                                            # max_length plus one to get the next tokens
+                                            max_length=BLOCK_SIZE + 1
+                                            ), batched=True)
+    dataset.set_format(type='torch', columns=['input_ids', 'attention_mask'])
+    dataset.save_to_disk(f'{split}_tokenized_tinystories')
+
+create_dataset('gpt2', block_size=BLOCK_SIZE, split='validation')
